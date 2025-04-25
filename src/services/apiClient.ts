429f5733452a1e5c297016fb9490e84078a013a5
rafaeltalarico <rpaulista80@gmail.com>
2025-04-25 17:26:57 -0300

    banco de dados local funcionando, mas online não

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,6 @@
 import { Product, RelatedProductData, SalesAnalysis } from "@/models/types";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = "http://localhost:8000";
 
 // Função para buscar um produto pelo nome ou ID
 export const getProductByNameOrId = async (
