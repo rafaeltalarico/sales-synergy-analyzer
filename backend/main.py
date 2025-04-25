c38c43af8b9ca46fb51019a3a7e94462186270d6
rafaeltalarico <rpaulista80@gmail.com>
2025-04-25 17:08:33 -0300

    alteração .env, mas o DB não está funcionando

--- backend/main.py
+++ backend/main.py
@@ -21,12 +21,13 @@ app.add_middleware(
 )
 
 DB_CONFIG = {
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-    "database": os.getenv("DB_NAME")
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "5842"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5432"),
+    "database": os.getenv("DB_NAME", "smart_metrics")
 }
+
 
 def get_db_connection():
     try:
